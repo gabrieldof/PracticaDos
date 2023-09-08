18e147b41ee4ec0914e81ae6b851f5561322ec75
Gabriel Furrer <gfurrer@fyo.com>
2023-09-07 22:06:54 -0300

    Ejercicio 4 Cambios en enemigo y Turno

--- PracticaDos/Ejercicio4/Enemigo.cs
+++ PracticaDos/Ejercicio4/Enemigo.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace PracticaDos.Ejercicio4
 {
-    public class Enemigo 
+    public class Enemigo : IMoverse
     {
         public int Vida { get; set; }
 
         public int Nivel { get; set; }
 
+        public void MoverseEjeX() {
+            Console.WriteLine("Enemigo se movió en el eje X");
+        }
+        public void MoverseEjeY() {
+            Console.WriteLine("Enemigo se movió en el eje Y");
+        }
 
     }
 }
--- PracticaDos/Ejercicio4/Turno.cs
+++ PracticaDos/Ejercicio4/Turno.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace PracticaDos.Ejercicio4
 {
-    internal class Turno
+    public static class Turno
     {
+        public static void Atacarse(IList<Personaje> personajes)
+        {
+            foreach (Personaje personaje in personajes)
+            { 
+                personaje.Atacar();
+            }
+        }
+        public static void MoverFichas(IMoverse ficha) {
+            ficha.MoverseEjeX();
+            ficha.MoverseEjeY();
+        }
+
     }
 }
