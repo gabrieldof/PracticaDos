e68b50c65b1fd71c8b22e1c813c606b1a3531a51
Gabriel Furrer <gfurrer@fyo.com>
2023-09-14 20:55:34 -0300

    Estructura ejercicio 5

---
+++ PracticaDos/Ejercicio5/Coche.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaDos.Ejercicio5
+{
+    
+    public class Coche : IVehiculo
+    {
+        public int gasolina;
+        public Coche(int gasolinaInicial)
+        {
+            gasolina = gasolinaInicial;
+        }
+
+        public bool CargarCombustible(int cantCombustible)
+        {
+            return true;
+        }
+
+        public void Conducir()
+        { }
+
+
+    }
+}
---
+++ PracticaDos/Ejercicio5/IVehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaDos.Ejercicio5
+{
+    public interface IVehiculo
+    {
+        public void Conducir();
+
+        public bool CargarCombustible(int cantCombustible);
+    }
+}
